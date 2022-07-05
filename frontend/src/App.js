344dc64dcb35ae1666a1f9ab12178e30658c2241
taouchikhtkhouloud <92301300+taouchikhtkhouloud@users.noreply.github.com>
2022-07-05 02:47:10 +0100

    side bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import Topbar from "./components/topbar/Topbar";
-
+import Sidebar from "./components/sidebar/Sidebar";
+import "./App.css";
 function App() {
   return (
     <div >
       <Topbar/>
+      <div className="container">
+        <Sidebar/>
+
+     <div className="others">others</div>
+    
     </div>
+      </div>
   );
 }
 
