b17e232c5f6fc43985cfdeda3fe92bc59001f0e6
taouchikhtkhouloud <92301300+taouchikhtkhouloud@users.noreply.github.com>
2022-07-06 01:50:49 +0100

    userlist

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,27 @@
 import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import Home from './pages/home/Home'
+import UserList from "./pages/userList/UserList";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div >
+    <Router>
       <Topbar/>
       <div className="container">
         <Sidebar/>
 
-        <Home />
+        
+        <Routes>
+          <Route exact path="/" element={<Home />}/>
+            
+ 
+          <Route path="/users" element={<UserList />}/>
+            
+          
+          </Routes>
       </div>
-    </div>
+      </Router>
   );
 }
 
